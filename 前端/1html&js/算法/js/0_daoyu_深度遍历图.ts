951336000c395ed0558ea6ece03165cf9c68a8e2
925577835 <925577835@qq.com>
2022-02-07 20:06:15 +0800

    算法小抄 岛屿

--- 前端/1html&js/算法/js/0_daoyu_深度遍历图.ts
+++ 前端/1html&js/算法/js/0_daoyu_深度遍历图.ts
@@ -1,4 +1,4 @@
-// https://labuladong.gitee.io/algo/1/7/
+// https://labuladong.gitee.io/algo/1/9/
 
 /**
  * 二维矩阵的 DFS 代码框架
@@ -230,37 +230,138 @@
  * 现在不让你计算岛屿的个数了，而是让你计算最大的那个岛屿的面积
  * dfs 函数淹没岛屿的同时，还应该想办法记录这个岛屿的面积
  */
-function maxAreaOfIsland(grid: number[][]): number {
-    let res = 0;
-    const m = grid.length, n = grid[0].length
+// function maxAreaOfIsland(grid: number[][]): number {
+//     let res = 0;
+//     const m = grid.length, n = grid[0].length
+//     for (let i = 0; i < m; i++) {
+//         for (let j = 0; j < n; j++) {
+//             if (grid[i][j] === 1) {
+//                 // 淹没岛屿，并更新最大岛屿面积
+//                 res = Math.max(res, dfs(grid, i, j));
+//             }
+//         }
+//     }
+//     return res
+// }
+
+// function dfs(grid: number[][], i: number, j: number): number {
+//     const m = grid.length, n = grid[0].length
+//     if (i < 0 || j < 0 || i >= m || j >= n) {
+//         // 超出索引边界
+//         return 0
+//     }
+//     if (grid[i][j] == 0) {
+//         // 已经是海水了
+//         return 0
+//     }
+//     // 将 (i, j) 变成海水
+//     grid[i][j] = 0;
+//     return dfs(grid, i + 1, j)
+//         + dfs(grid, i, j + 1)
+//         + dfs(grid, i - 1, j)
+//         + dfs(grid, i, j - 1) + 1;
+// }
+
+// const grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+// console.log(maxAreaOfIsland(grid))
+
+
+/**
+ * 力扣第 1905 题「统计子岛屿」
+ * https://leetcode-cn.com/problems/count-sub-islands/
+ * 这道题的关键在于，如何快速判断子岛屿？肯定可以借助 Union Find 并查集算法 来判断，不过本文重点在 DFS 算法，就不展开并查集算法了。
+ * 什么情况下 grid2 中的一个岛屿 B 是 grid1 中的一个岛屿 A 的子岛？
+   当岛屿 B 中所有陆地在岛屿 A 中也是陆地的时候，岛屿 B 是岛屿 A 的子岛。
+   反过来说，如果岛屿 B 中存在一片陆地，在岛屿 A 的对应位置是海水，那么岛屿 B 就不是岛屿 A 的子岛。
+ * 那么，我们只要遍历 grid2 中的所有岛屿，把那些不可能是子岛的岛屿排除掉，剩下的就是子岛。
+ */
+
+// function countSubIslands(grid1: number[][], grid2: number[][]): number {
+//     const m = grid1.length, n = grid1[0].length;
+//     for (let i = 0; i < m; i++) {
+//         for (let j = 0; j < n; j++) {
+//             if (grid1[i][j] == 0 && grid2[i][j] == 1) {
+//                 // 这个岛屿肯定不是子岛，淹掉
+//                 dfs(grid2, i, j);
+//             }
+//         }
+//     }
+//     // 现在 grid2 中剩下的岛屿都是子岛，计算岛屿数量
+//     let res = 0;
+//     for (let i = 0; i < m; i++) {
+//         for (let j = 0; j < n; j++) {
+//             if (grid2[i][j] == 1) {
+//                 res++;
+//                 dfs(grid2, i, j);
+//             }
+//         }
+//     }
+//     return res;
+// }
+
+// // 从 (i, j) 开始，将与之相邻的陆地都变成海水
+// function dfs(grid: number[][], i: number, j: number) {
+//     const m = grid.length, n = grid[0].length;
+//     if (i < 0 || j < 0 || i >= m || j >= n) {
+//         return;
+//     }
+//     if (grid[i][j] == 0) {
+//         return;
+//     }
+
+//     grid[i][j] = 0;
+//     dfs(grid, i + 1, j);
+//     dfs(grid, i, j + 1);
+//     dfs(grid, i - 1, j);
+//     dfs(grid, i, j - 1);
+// }
+
+// const grid1 = [[1, 0, 1, 0, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [1, 0, 1, 0, 1]], grid2 = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [0, 1, 0, 1, 0], [0, 1, 0, 1, 0], [1, 0, 0, 0, 1]]
+// console.log(countSubIslands(grid1, grid2))
+
+/**
+ * 力扣第 694 题「不同的岛屿数量」 plus题
+ * 0 表示海水，1 表示陆地，这次让你计算 不同的 (distinct) 岛屿数量
+ * 如果想把岛屿转化成字符串，说白了就是序列化，序列化说白了就是遍历嘛，前文 二叉树的序列化和反序列化 讲了二叉树和字符串互转，这里也是类似的。
+ * 对于形状相同的岛屿，如果从同一起点出发，dfs 函数遍历的顺序肯定是一样的。
+ * 用 1, 2, 3, 4 代表上下左右，用 -1, -2, -3, -4 代表上下左右的撤销
+ * 只要每次使用 dfs 遍历岛屿的时候生成这串数字进行比较，就可以计算到底有多少个不同的岛屿了。
+ */
+
+function numDistinctIslands(grid: number[][]) {
+    const m = grid.length, n = grid[0].length;
+    // 记录所有岛屿的序列化结果
+    const islands = new Set();
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 1) {
-                // 淹没岛屿，并更新最大岛屿面积
-                res = Math.max(res, dfs(grid, i, j));
+            if (grid[i][j] == 1) {
+                // 淹掉这个岛屿，同时存储岛屿的序列化结果
+                const strArr = []
+                // 初始的方向可以随便写，不影响正确性
+                dfs(grid, i, j, strArr, 666);
+                islands.add(strArr.toString());
             }
         }
     }
-    return res
+    // 不相同的岛屿数量
+    return islands.size;
 }
 
-function dfs(grid: number[][], i: number, j: number): number {
-    const m = grid.length, n = grid[0].length
-    if (i < 0 || j < 0 || i >= m || j >= n) {
-        // 超出索引边界
-        return 0
+function dfs(grid: number[][], i: number, j: number, strArr: number[], dir: number) {
+    const m = grid.length, n = grid[0].length;
+    if (i < 0 || j < 0 || i >= m || j >= n
+        || grid[i][j] == 0) {
+        return;
     }
-    if (grid[i][j] == 0) {
-        // 已经是海水了
-        return 0
-    }
-    // 将 (i, j) 变成海水
+    // 前序遍历位置：进入 (i, j)
     grid[i][j] = 0;
-    return dfs(grid, i + 1, j)
-        + dfs(grid, i, j + 1)
-        + dfs(grid, i - 1, j)
-        + dfs(grid, i, j - 1) + 1;
-}
+    strArr.push(dir)
 
-const grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
-console.log(maxAreaOfIsland(grid))
+    dfs(grid, i - 1, j, strArr, 1); // 上
+    dfs(grid, i + 1, j, strArr, 2); // 下
+    dfs(grid, i, j - 1, strArr, 3); // 左
+    dfs(grid, i, j + 1, strArr, 4); // 右
+
+    // 后序遍历位置：离开 (i, j)
+    strArr.push(-dir)
+}
